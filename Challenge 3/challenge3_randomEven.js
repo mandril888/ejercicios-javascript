649800bb402b6ec96881fc90a8570d45d2137a1a
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-06 17:06:27 +0200

    ordered files

--- Challenge 3/challenge3_randomEven.js
+++ Challenge 3/challenge3_randomEven.js
@@ -7,8 +7,10 @@ function randomEven(){
 	var max = 100;
 	var min = 0;
 	var randNum = Math.floor(Math.random() * (max - min)) + min;
-	for (var i = 0; i < randNum; i++) {
-		randNum
+	var result = '';
+	for (var i = 0; i <= randNum; i+=2) {
+		result += i + ' ';
 	}
+	console.log(randNum);
 	return result;
 }
---
+++ Challenge 3/challenge3_randomOdd.js
@@ -0,0 +1,9 @@
+/*randomOdd()
+
+Randomizes a number (range 0..100), then prints all the odd numbers
+from 40 to that one. If the number was smaller than 40 nothing
+should be printed*/
+
+function randomOdd(){
+	
+}
