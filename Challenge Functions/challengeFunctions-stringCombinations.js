cc9fd6b9eae419a40e72880122172ac64e200e1f
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-09 14:25:08 +0200

    challenge functions upload2

--- Challenge Functions/challengeFunctions-stringCombinations.js
+++ Challenge Functions/challengeFunctions-stringCombinations.js
@@ -1,4 +1,24 @@
 /*stringCombinations
 
-Write a JavaScript function that generates all combinations of a string*/
+Write a JavaScript function that generates all combinations of a string
 
+Example string : 'dog' 
+Expected Output : d,do,dg,dog,o,og,g */
+
+function combinations( string ){
+    var result = [];
+    function loop( start, depth, prefix ){
+        for(var i=start; i<string.length; i++){
+            var next = prefix+string[i];
+            if (depth > 0){
+                loop(i+1,depth-1,next);
+            } else{
+                result.push(next);
+            }
+        }
+    }
+    for(var i=0; i<string.length; i++){
+        loop(0,i,'');
+    }
+    return result;
+}
